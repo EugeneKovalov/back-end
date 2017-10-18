c11657f7f5d81fd57f771a7d4ba5813ae40af627
Eugene Kovalov <31100178+eugenekovalov@users.noreply.github.com>
2017-10-18 17:35:52 +0300

    Filemanager Fixes

--- september/HW/filesystem/file_manager/fileManager.php
+++ september/HW/filesystem/file_manager/fileManager.php
@@ -1,25 +1,63 @@
 <?php
-//1. https://github.com/EugeneKovalov/back-end/blob/master/september/HW/filesystem/file_manager/fileManager.php#L80 - если здесь (и в rename_file, и в edit_file инпутах) - передавать не просто имя, а полный путь к файлу относительно корня, то можно будет избавиться от циклов тут https://github.com/EugeneKovalov/back-end/blob/master/september/HW/filesystem/file_manager/fileManager.php#L145, тут https://github.com/EugeneKovalov/back-end/blob/master/september/HW/filesystem/file_manager/fileManager.php#L115, и тут https://github.com/EugeneKovalov/back-end/blob/master/september/HW/filesystem/file_manager/fileManager.php#L160
-//
-//2. https://github.com/EugeneKovalov/back-end/blob/master/september/HW/filesystem/file_manager/fileManager.php#L149 - переименование происходит, когда список файлов уже выведен, поэтому чтобы увидеть новое имя файла человеку придется обновить страницу. Аналогично с удалением. Вообще по-хорошему все реальные операции над файлами - обработки форм переименования, удаления, редактирования - делать еще до того, как мы собираем информацию о файлах.
-
-
 define('DS', DIRECTORY_SEPARATOR);
 // Определим корневую директорию
 $base = $_SERVER['DOCUMENT_ROOT'];
 // Определяем путь выбранной директории относительно корня
 $path = '';
 
+// EDIT FILE
+if (isset($_POST['edit_button'])): ?>
+    <form method="post">
+        <p><?= $_POST['editFile'] ?></p>
+        <input type="hidden" name="file" value="<?= $_POST["editFile"] ?>">
+        <?
+        $content = file_get_contents($_POST['editFile']);
+        echo "<textarea rows='10' name='content'>" .
+            file_get_contents($base.$_GET['dir'].DS.$_POST['editFile']) . "</textarea>";
+        ?>
+        <input name="submit_edit" type="submit" value="submit edit">
+    </form>
+<?endif;
+
+// EDIT SUBMIT
+if (isset($_POST['submit_edit'])):
+    $file = $base.$_GET['dir'].DS.$_POST['file'];
+    $openFile = fopen($file,"w+");
+    $newContent = $_POST['content'];
+    fwrite($openFile, $newContent);
+    fclose($openFile);
+endif;
+
+// RENAME FILE
+if (isset($_POST['rename_button'])):?>
+    <form method="post">
+        <p><?= $_POST['renameFile'] ?></p>
+        <input name="new_file" value="" placeholder="Rename">
+        <input type="hidden" name="old_file" value="<?= $_POST["renameFile"] ?>">
+        <input name="submit_rename" type="submit" value="submit rename">
+    </form>
+<?endif;
+
+// SUBMIT RENAME FILE
+if (isset($_POST['submit_rename'])):
+    $fileName = $base.$_GET['dir'].DS;
+    rename($fileName.$_POST['old_file'], $fileName.$_POST['new_file']);
+endif;
+
 // REMOVE FILE
 if (isset($_POST['remove_button'])) {
-    $fileName = $base.DS.$path.DS.$_POST['removeFile'];
+    $fileName = $base.$_GET['dir'].DS.$_POST['removeFile'];
 
-    if(is_dir($fileName)) {
-        echo $fileName . " dir has been deleted.";
-        rmdir($fileName);
-    } else {
-        echo $fileName . " file has been deleted.";
+    if(is_file($fileName)) {
+        echo $_POST['removeFile'] . " file has been deleted.";
         unlink($fileName);
+    } else {
+        $n = $_POST['removeFile'];
+        $n = substr($n, 14);
+        $n = substr($n, 0, strpos($n, '"'));
+
+        echo $_POST['removeFile'] . " dir has been deleted.";
+        rmdir($base.$n);
     }
 }
 
@@ -37,8 +75,6 @@ if (!$path) {
 $files = array_diff($files, $removeParts);
 // Формируем данные для вывода
 $result = [];
-
-//var_dump($_POST);
 
 foreach ($files as $file) {
     $name = $file;
@@ -122,54 +158,6 @@ foreach ($files as $file) {
                         <td><?= $file['edited_at'] ?></td>
                     </tr>
                 <? endforeach; ?>
-
-                <!--            EDIT FILE-->
-                <?if (isset($_POST['edit_button'])): ?>
-
-                    <form method="post">
-                        <p><?= $_POST['editFile'] ?></p>
-
-                        <? $fileName = $_POST['editFile'];
-                        foreach ($result as $item) {
-                            if ($fileName == $item['name']) {
-                                file_put_contents($item['name'], $_POST['editFile']);
-                                $content = file_get_contents(basename($item['name']));
-                                echo "<textarea name='html'>" .
-                                    htmlspecialchars($content)
-                                    . "</textarea>";
-                                ?>
-                                <input name="submit_edit" type="submit" value="submit edit">
-                                <?
-                                break;
-                            }
-                        }
-                        ?>
-                    </form>
-                <?endif;?>
-
-                <!--            RENAME FILE-->
-                <?if (isset($_POST['rename_button'])): ?>
-                    <form method="post">
-                        <p><?= $_POST['renameFile'] ?></p>
-                        <input name="new_file" value="" placeholder="Rename">
-                        <input type="hidden" name="old_file" value="<?= $_POST["renameFile"] ?>">
-                        <input name="submit_rename" type="submit" value="submit rename">
-                    </form>
-                <?endif;?>
-
-                <!--            SUBMIT RENAME FILE-->
-                <?if (isset($_POST['submit_rename'])):
-                    $fileName = $_POST['old_file'];
-                    foreach ($result as $item) {
-                        if ($fileName == $item['name']) {
-                            $abs = $item['path'];
-
-                            rename($item['path'], $base.DS.$path.DS.$_POST['new_file']);
-                            echo $fileName . " file has been renamed.";
-                            break;
-                        }
-                    }
-                endif;?>
                 </tbody>
             </table>
         </div>
