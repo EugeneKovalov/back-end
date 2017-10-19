38cf9ba0bbd9e77916446bf45b00dfda19d74ab3
Eugene Kovalov <31100178+eugenekovalov@users.noreply.github.com>
2017-10-19 21:00:31 +0300

    Objects added

--- october/Obj/obj.php
+++ october/Obj/obj.php
@@ -1,140 +1,377 @@
 <?php
 
-class CarOwner
+// 1
+class Task1
 {
-    public $owner = '';
+    /** @var string */
+    public $name;
+    /** @var int */
+    public $age;
+    /** @var int */
+    public $salary;
 }
-//
-//$toyota = new Car();
-//
-//$toyota -> price = 22;
-//
-//echo $toyota -> price.PHP_EOL;
-//
-//class Human
-//{
-//    /** @var  int */
-//    private $age = 20;
-//    /** @var  string */
-//    private $name;
-//
-//    /**
-//     * @param int $age
-//     * @return void|bool
-//     */
-//    public function setAge(int $age) {
-//        if ($age < 18) {
-//            echo "u too young".PHP_EOL;
-//            return;
-//        }
-//        $this->age = $age;
-//    }
-//
-//    /**
-//     * @return int
-//     */
-//    public function getAge($formatted = false) {
-//        if ($formatted) {
-//            return "Мне {$this->age} лет";
-//        }
-//        return $this->age;
-//    }
-//
-//    public function setName(string $name) {
-//        $this->name = $name;
-//    }
-//    public function getName() {
-//        return $this->name;
-//    }
-//
-//    private function doubleAge(int $age) {
-//        echo "Через 20 лет мне будет. " . ($this->age + 20).PHP_EOL;
-//    }
-//
-//    function showName() {
-//        echo "{$this->name}, I am {$this->age} old.".PHP_EOL;
-//        $this->doubleAge(21);
-//        return;
-//    }
-//}
-//
-//$human1 = new Human();
-//$human1 -> setName('Bill');
-//$human1 -> setAge(13);
-//
-//$human2 = new Human();
-//$human2 -> setName('David');
-//$human2 -> setAge(30);
-//$human2 -> getAge(true);
-//
-//$human3 = $human2;
-//$human3->setName('Jack');
-//$human3->showName();
-//
-//$human2->showName();
-//
-//$human4 = clone($human2);
-//$human4->setName('Stas');
-//$human2->showName();
-//
-///** @var Human $human5 */
-//$human5 = serialize($human2);
-//$human5 = unserialize($human2);
 
+$ivanTask1 = new Task1();
+$ivanTask1->name = "Иван";
+$ivanTask1->age = 25;
+$ivanTask1->salary = 1000;
 
-class Car {
-    private $price;
-    private $color;
-    protected $brand;
+$vasyaTask1 = new Task1();
+$vasyaTask1->name = "Вася";
+$vasyaTask1->age = 26;
+$vasyaTask1->salary = 2000;
 
-    function __construct($brand, $color, $price) {
+echo "Cумма зарплат Ивана и Васи = " . ($vasyaTask1->salary + $ivanTask1->salary) .PHP_EOL;
+echo "Cумма возрастов Ивана и Васи = " . ($vasyaTask1->age + $ivanTask1->age) .PHP_EOL;
 
-        $this->brand = $brand;
-        $this->color = $color;
-        $this->price = $price;
+// 2
+class Task2
+{
+    /** @var string */
+    private $name;
+    /** @var int */
+    private $age;
+    /** @var int */
+    private $salary;
 
-        echo "New Car".PHP_EOL;
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
     }
 
-    private function __clone()
+    /**
+     * @param string $name
+     */
+    public function setName(string $name)
     {
-        // TODO: Implement __clone() method.
+        $this->name = $name;
     }
 
-    function __destruct()
+    /**
+     * @return int
+     */
+    public function getAge(): int
     {
-        // TODO: Implement __destruct() method.
-        echo 'Destructed!'.PHP_EOL;
+        return $this->age;
+    }
+
+    /**
+     * @param int $age
+     */
+    public function setAge(int $age)
+    {
+        if ($this->checkAge($age)) {
+            $this->age = $age;
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getSalary(): int
+    {
+        return $this->salary;
+    }
+
+    /**
+     * @param int $salary
+     */
+    public function setSalary(int $salary)
+    {
+        $this->salary = $salary;
+    }
+
+    // 3.
+    private function checkAge($age): bool
+    {
+        if ($age < 1 || $age >= 100)
+        {
+            echo "Некорректный возраст".PHP_EOL;
+            return false;
+        }
+        return true;
     }
 }
 
-$ford = new Car('Ford', 'Red', '15000');
+$ivanTask2 = new Task2();
+$vasyaTask2 = new Task2();
 
-var_dump($ford);
+$ivanTask2->setName('Иван');
+$vasyaTask2->setName('Вася');
 
-//$ford = null;
+$ivanTask2->setAge(25);
+$vasyaTask2->setAge(26);
 
+$ivanTask2->setSalary(1000);
+$vasyaTask2->setSalary(2000);
 
-class Truck extends Car {
-    private $weight;
+echo "Cумма зарплат Ивана и Васи = " . ($vasyaTask2->getSalary() + $ivanTask2->getSalary()) .PHP_EOL;
+echo "Cумма возрастов Ивана и Васи = " . ($vasyaTask2->getAge() + $ivanTask2->getAge()) .PHP_EOL;
 
-    function setWeight(int $weight) {
-        $this->weight = $weight;
-    }
+$oldestMan = new Task2();
+$oldestMan->setAge(120);
+$oldestMan->setAge(0);
 
-    function __construct($brand, $color, $price, $weight)
+//echo $oldestMan->getAge();
+
+// 4
+class Task4
+{
+    /** @var string */
+    private $name;
+    /** @var int */
+    private $age;
+    /** @var int */
+    private $salary;
+
+    public function __construct($name, $age, $salary)
     {
-        parent::__construct($brand, $color, $price);
-        $this->setWeight($weight);
+        $this->name = $name;
+        $this->age = $age;
+        $this->salary = $salary;
     }
 
-    public function displayParams() {
-        echo "Brand: {$this->brand}";
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSalary(): int
+    {
+        return $this->salary;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAge(): int
+    {
+        return $this->age;
     }
 }
 
-$kraz = new Truck('Kraz', 'Black', '20000', 4000);
-$kraz->setWeight(3000);
-$kraz->displayParams();
+$dimaTask4 = new Task4("Dima", 25, 1000);
 
-var_dump($kraz);
+echo "Cумма возраста и зарплаты Димы = " . ($dimaTask4->getAge() + $dimaTask4->getSalary()).PHP_EOL;
+
+// 5
+class User
+{
+    protected $name;
+    protected $age;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+}
+
+class Worker extends User
+{
+    private $salary;
+
+    /**
+     * @return mixed
+     */
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+
+    /**
+     * @param mixed $salary
+     */
+    public function setSalary($salary)
+    {
+        $this->salary = $salary;
+    }
+}
+
+$ivanWorker = new Worker();
+$ivanWorker->setName("Иван");
+$ivanWorker->setAge(25);
+$ivanWorker->setSalary(1000);
+
+$vasyaWorker = new Worker();
+$vasyaWorker->setName("Вася");
+$vasyaWorker->setAge(26);
+$vasyaWorker->setSalary(2000);
+
+echo "Cумма зарплат Ивана и Васи = " . ($vasyaWorker->getSalary() + $ivanWorker->getSalary()) .PHP_EOL;
+
+class Student extends User
+{
+    private $stipend;
+    private $course;
+
+    /**
+     * @return mixed
+     */
+    public function getStipend()
+    {
+        return $this->stipend;
+    }
+
+    /**
+     * @param mixed $stipend
+     */
+    public function setStipend($stipend)
+    {
+        $this->stipend = $stipend;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCourse()
+    {
+        return $this->course;
+    }
+
+    /**
+     * @param mixed $course
+     */
+    public function setCourse($course)
+    {
+        $this->course = $course;
+    }
+}
+
+// 6
+class Driver extends Worker
+{
+    /** @var  int */
+    private $drivingExperience;
+    private $drivingCategory = ['A', 'B', 'C'];
+}
+
+// 7
+class Form
+{
+    function input($attributes)
+    {
+        echo "<input type=\"$attributes[0]\" " . "value=\"$attributes[1]\">".PHP_EOL;
+    }
+
+    function submit($attributes)
+    {
+        echo "<input type=\"$attributes[0]\" " . "value=\"$attributes[1]\">".PHP_EOL;
+    }
+
+    function password($attributes)
+    {
+        echo "<input type=\"$attributes[0]\" " . "value=\"$attributes[1]\">".PHP_EOL;
+    }
+
+    function textArea($attributes)
+    {
+        echo "<textarea placeholder=\"$attributes[0]\">$attributes[1]</textarea>".PHP_EOL;
+    }
+
+    function open($attributes)
+    {
+        echo "<form action=\"$attributes[0]\" " . "method=\"$attributes[1]\">".PHP_EOL;
+    }
+
+    function close()
+    {
+        echo "</form>".PHP_EOL;
+    }
+}
+
+$form = new Form();
+$form->open(["index.php", "POST"]);
+$form->input(["text", "!!!"]);
+$form->password(["password", "!!!"]);
+$form->submit(["submit", "go"]);
+$form->textArea(["123", "!!!"]);
+$form->close();
+
+// 8
+class Cookie
+{
+    function setCookie($name, $value)
+    {
+        setcookie($name, $value);
+    }
+
+    function getCookie($name)
+    {
+        return $_COOKIE[$name];
+    }
+
+    function delCookie($name)
+    {
+        setcookie($name, "", time() - 3600);
+    }
+}
+
+class Session
+{
+    public function __construct()
+    {
+        session_start();
+    }
+
+    function createSessionVar($name)
+    {
+        $_SESSION[$name] = $name;
+    }
+
+    function getSessionVar($name)
+    {
+        return $_SESSION[$name];
+    }
+
+    function delSessionVar($name)
+    {
+        unset($_SESSION[$name]);
+    }
+
+    function checkSessionVar($name)
+    {
+        if (isset($_SESSION['errors']))
+        {
+            echo "Сессия существует".PHP_EOL;
+        }
+        else {
+            echo "Сессия не существует".PHP_EOL;
+        }
+    }
+}
